4e9b950231cf0b1021e17042440fa42d2207a332
Dio Tendean <tendeanclaudio1234@gmail.com>
2023-05-24 13:01:15 +0800

    Initial Commit

---
+++ LoadImagefromJson/LoadImagefromJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoadImagefromJsonApp.swift
+//  LoadImagefromJson
+//
+//  Created by Claudio Tendean on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LoadImagefromJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
